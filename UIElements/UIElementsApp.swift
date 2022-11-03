9cf8fa0fe067d3d14a0caa6018ffbffc1ee8dfd6
Pttr <draivv@yandex.ru>
2022-11-03 21:24:21 +0300

    Initial Commit

---
+++ UIElements/UIElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIElementsApp.swift
+//  UIElements
+//
+//  Created by P Z on 03/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct UIElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
